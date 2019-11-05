59a3ac275a84a85eb449f763b25a9f2010baaa7c
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-11-06 00:51:36 +0900

    add: kazuma_info & assert

---
+++ task_A/a_5.py
@@ -0,0 +1,5 @@
+kazuma_info = ["Kazuma", "Takahashi", 35]
+print(f"Name: {kazuma_info[1]} {kazuma_info[0]} Age: {kazuma_info[2]}")
+
+assert isinstance(kazuma_info[0], str)
+assert isinstance(kazuma_info[2], int)
