c7af794a2c976bfc7ee718fcb23171b3f880c2bf
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-11-06 16:20:20 +0900

    add: dice_iterative_try

--- task_B/dice_iterative_try.py
+++ task_B/dice_iterative_try.py
@@ -1,4 +1,9 @@
-for column in range(1, 10):
-    for line in range(1, 10):
-        print(column * line, end="")
-    print()
+import random
+surface = input("サイコロの面の数は?:")
+count = input("何回振りますか?:")
+
+result = []
+for challenge in range(int(count)):
+    value = random.randint(1, int(surface))
+    result.append(value)
+print(result)
