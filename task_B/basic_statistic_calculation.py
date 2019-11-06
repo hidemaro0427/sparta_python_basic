89cde54d28d3f5d3452e8ea59494aa9a5b7240d7
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-11-06 15:33:48 +0900

    add: function maximum

--- task_B/basic_statistic_calculation.py
+++ task_B/basic_statistic_calculation.py
@@ -4,9 +4,17 @@ def sum_value(numbers):
         total += number
     return total
 
+def maximum(numbers):
+    max = 0
+    for number in numbers:
+        if max < number:
+            max = number
+    return max
+
 numbers = input("データを入力してください(スペース区切り) >")
 list = []
 for number in numbers.split(" "):
     list.append(int(number))
 
 print(sum_value(list))
+print(maximum(list))
